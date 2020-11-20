badd8cafb0682e96f3512d06b75f44398d683933
maxsossin <sossinbergs@gmail.com>
2020-11-20 17:27:55 -0500

    Removed imports
    removed imports that were not needed

--- qa327_test/frontend/test_404.py
+++ qa327_test/frontend/test_404.py
@@ -1,10 +1,6 @@
 import pytest
 from seleniumbase import BaseCase
 
-from qa327_test.conftest import base_url
-from unittest.mock import patch
-from qa327.models import db, User
-from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class PageNotFoundTest(BaseCase):
